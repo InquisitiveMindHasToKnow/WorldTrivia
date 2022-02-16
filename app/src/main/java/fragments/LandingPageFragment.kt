721434e7cf2a5cbecc0dc78c20c284e341acf1df
InquisitiveMindHasToKnow <raymondomardev@gmail.com>
2022-02-15 22:11:48 -0500

    Stopped user from going to TriviaFragment without entering a name. Keyboard enter button changed to done to remove ability to add additional lines to edittext.

--- app/src/main/java/fragments/LandingPageFragment.kt
+++ app/src/main/java/fragments/LandingPageFragment.kt
@@ -1,19 +1,37 @@
 package fragments
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import org.ohmstheresistance.worldtrivia.R
+import org.ohmstheresistance.worldtrivia.databinding.LandingPageFragmentBinding
 
 class LandingPageFragment : Fragment() {
+
+    private lateinit var landingPageFragmentBinding: LandingPageFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.landing_page_fragment, container, false)
+
+        landingPageFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.landing_page_fragment, container, false)
+
+        landingPageFragmentBinding.startButton.setOnClickListener {
+
+            if(landingPageFragmentBinding.playerNameEdittext.text.toString().isEmpty()){
+                Toast.makeText(context, "Please enter your name!", Toast.LENGTH_SHORT).show()
+            }else{
+                findNavController().navigate(R.id.action_landingPageFragment_to_triviaFragment)
+            }
+        }
+        return landingPageFragmentBinding.root
     }
 }
